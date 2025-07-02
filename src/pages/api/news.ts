f46e50914e2a729b10ab6ccbeedfd27a3847e7bb
swayam agrawal <anshujatni22@gmail.com>
2025-07-02 09:31:26 +0530

    tried fixing api

--- src/pages/api/news.ts
+++ src/pages/api/news.ts
@@ -1,10 +1,10 @@
-// pages/api/news.ts
+// src/pages/api/news.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { category = "general" } = req.query;
 
-  const apiKey = process.env.NEWS_API_KEY;
+  const apiKey = process.env.NEXT_PUBLIC_NEWS_API_KEY;
   const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apiKey}`;
 
   try {
